9ba74d8d2dcebf0819145d6bf0910a296c495460
RakibPranto <rakibpranto123@gmail.com>
2022-08-22 21:58:39 +0600

    commit2

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,115 +13,136 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff17181f),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 100),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(children: [
-                Container(
-                  height: 300,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(120),
-                  ),
-                  child: Container(
-                    margin: const EdgeInsets.all(2),
-                    height: 200,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      border:
-                          Border.all(color: const Color(0xff17181f), width: 10),
-                      borderRadius: BorderRadius.circular(120),
-                      image: const DecorationImage(
-                        image: AssetImage(
-                          "assets/images/homepic.jpg",
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xff17181f),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 8,
+                  child: Stack(children: [
+                    Container(
+                      height: 300,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(120),
+                      ),
+                      child: Container(
+                        margin: const EdgeInsets.all(2),
+                        height: 200,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color(0xff17181f), width: 10),
+                          borderRadius: BorderRadius.circular(120),
+                          image: const DecorationImage(
+                            image: AssetImage(
+                              "assets/images/homepic.jpg",
+                            ),
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                        fit: BoxFit.cover,
                       ),
                     ),
+                    const Positioned(
+                        top: 100, left: 100, child: ArcTextWidget())
+                  ]),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "Choose a ",
+                            style: TextStyle(color: Colors.white54),
+                          ),
+                          Text(
+                            "celebrity ",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            "look",
+                            style: TextStyle(color: Colors.white54),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Center(
+                        child: Text(
+                          "to detect the makeup or snap",
+                          style: TextStyle(color: Colors.white54),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "your ",
+                            style: TextStyle(color: Colors.white54),
+                          ),
+                          Text(
+                            "picture ",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            "for applience",
+                            style: TextStyle(color: Colors.white54),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                const Positioned(top: 100, left: 100, child: ArcTextWidget())
-              ]),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    "Choose a ",
-                    style: TextStyle(color: Colors.white54),
+                Expanded(
+                  flex: 2,
+                  child: SizedBox(
+                    child: ListScrollWidget(),
                   ),
-                  Text(
-                    "celebrity ",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(
-                    "look",
-                    style: TextStyle(color: Colors.white54),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const Center(
-                child: Text(
-                  "to detect the makeup or snap",
-                  style: TextStyle(color: Colors.white54),
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    "your ",
-                    style: TextStyle(color: Colors.white54),
-                  ),
-                  Text(
-                    "picture ",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Text(
-                    "for applience",
-                    style: TextStyle(color: Colors.white54),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.2,
-                child: ListScrollWidget(),
-              ),
-              ActionChip(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                  backgroundColor: Colors.white,
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => MainPage(),
-                    ));
-                  },
-                  label: const Text(
-                    "Get Started",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15),
-                  )),
-              const SizedBox(
-                height: 15,
-              ),
-            ],
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: ActionChip(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 20),
+                      backgroundColor: Colors.white,
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => MainPage(),
+                        ));
+                      },
+                      label: const Text(
+                        "Get Started",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15),
+                      )),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+              ],
+            ),
           ),
         ),
       ),
